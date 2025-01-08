d5c4457417c5990715bedf7f62f28ccd03833432
knight <knight@Ubuntu.myguest.virtualbox.org>
2025-01-08 19:37:36 -0300

    Remove element-fix

--- src/RemoveElement/Program.java
+++ src/RemoveElement/Program.java
@@ -1,25 +1,24 @@
 package RemoveElement;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Program {
     public static void main(String[] args) {
-        int[] numbers = new int[]{1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 7};
 
-        int[] toRemove = new int[]{2, 5, 7};
+        int[] numbers = new int[]{1, 2, 2, 3, 4, 4, 5, 6, 7, 7, 7};
+        Set<Integer> toRemove = Set.of(2, 5, 7);
 
         List<String> list = new ArrayList<>();
-        int count = 0;
-        for(int value : toRemove) {
-            count += (int) Arrays.stream(numbers).filter(number -> number == value).count();
 
-            list = Arrays.stream(numbers).filter(number -> number != value)
-                    .mapToObj(number -> String.valueOf(number))
-                    .collect(Collectors.toList());
+        int count = 0;
+        for(int number : numbers){
+            if(!toRemove.contains(number)){
+                list.add(String.valueOf(number));
+            }else{
+                count++;
+            }
         }
+
         for(int i = 0; i < count; i++){
             list.add("_");
         }
